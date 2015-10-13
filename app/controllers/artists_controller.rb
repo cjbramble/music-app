8d26eda5dcbe1768cfd7571de0057267c05a8203
Chet Bramble <cjbramble@realdigitalmedia.com>
2015-10-13 14:20:32 -0400

    Generated songs, artists controllers and views.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,25 @@
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    @artist.save
+    redirect_to @artist
+  end
+
+  private
+    def artist_params
+      params.require(:artist).permit(:name)
+    end
+  end
