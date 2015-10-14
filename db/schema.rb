45fcc8ddb74202f7740c2941675394906efe57d3
Chet Bramble <cjbramble@realdigitalmedia.com>
2015-10-14 11:57:01 -0400

    Switched to mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,16 @@
 ActiveRecord::Schema.define(version: 20151012133726) do
 
   create_table "artists", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "artist_id"
+    t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "artist_id",  limit: 4
   end
 
 end
