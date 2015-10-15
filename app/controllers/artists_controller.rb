76c88e3756970f555afcebbc75c9b394a95c2b44
Todd Perkins <tperkins@realdigitalmedia.com>
2015-10-15 09:27:41 -0400

    RDoc examples
    Make sure to run bundle install

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,17 +1,32 @@
 class ArtistsController < ApplicationController
 
+  # List all artists
+  #
   def index
     @artists = Artist.all
   end
 
+  # Show specific artist along with songs that belong to that artist
+  #
+  # @param id [Symbol] :integer
+  # @return [View]
+  #
   def show
     @artist = Artist.where(id: params[:id]).first
   end
 
+  # New artist form
+  #
   def new
     @artist = Artist.new
   end
 
+  # Process and create new artist
+  #
+  # @param name [Symbol] :string
+  # @param artist_id [Symbol] :integer
+  # @return [Redirect]
+  #
   def create
     @artist = Artist.new(artist_params)
 
