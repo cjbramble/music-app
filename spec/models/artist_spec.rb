af841510b4dc49d58699ddd45c13994e6979ccaf
Chet Bramble <cjbramble@realdigitalmedia.com>
2015-10-30 16:05:53 -0400

    Added tests for model validations.

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Artist, :type => :model do
-  it "should have a name" do
-    expect(Artist.new(name: 'foo')).to be_valid
+
+  let(:artist) { Artist.create(name: 'foo') }
+  let(:long_name) { (0...151).map { 'x' }.join }
+
+  it "should be valid" do
+    expect(artist).to be_valid
+  end
+
+  it "should save the name" do
+    expect(artist.name).to eq('foo')
+  end
+
+  it "should not have name longer than 150 characters" do
+    expect(artist.update(name: long_name)).to be false
+  end
+
+  context "has associated songs" do
+    it "should have songs" do
+      expect { artist.songs.create(title: 'foo') }.to change { artist.songs.length }.by(1)
+    end
   end
 end
