ba842cfd05229f99c55570df6e89704948f72ccd
cjbrambo <cjbramble@gmail.com>
2015-11-06 17:57:27 -0500

    Got first artist on welcome index to show artist AR id

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @artists = random_artists_for_welcome_page
   end
-end
+
+  private
+    def random_artists_for_welcome_page
+      rand_id = rand(Artist.count).to_i
+      rand_record = Artist.where("id >= ?", rand_id).limit(1)
+    end
+  end
