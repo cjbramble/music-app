e9dbc1597584586564cd7027f51c4e79805998b2
Chet Bramble <cjbramble@gmail.com>
2015-11-11 19:55:35 -0500

    Added random artists to welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,8 @@ class WelcomeController < ApplicationController
   end
 
   private
-    def random_artists_for_welcome_page
-      rand_id = rand(Artist.count).to_i
-      rand_record = Artist.where("id >= ?", rand_id).limit(1)
-    end
+  
+  def random_artists_for_welcome_page
+    Artist.where("id in (?)", Artist.pluck(:id).shuffle[0..2])
   end
+end
