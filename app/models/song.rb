46b8a211fdaeeb1f01c5a9eaac34216d8b7e4f52
Chet Bramble <cjbramble@gmail.com>
2015-11-21 16:18:52 -0500

    Replaced song to artist association with song through album to artist association.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,9 @@
 # validates name under maximum allowed characters
 #
 class Song < ActiveRecord::Base
-  belongs_to :artist
   belongs_to :album
+  has_one :artist, through: :album
+
   validates :title, presence: true, length: { maximum: 150 }
+  validates :album_id, presence: true
 end
