63297e63e6a6ed35818967d01f1ffe8cd01e4c3b
Chet Bramble <cjbramble@gmail.com>
2015-11-30 20:43:29 -0500

    Revert "Replaced song to artist association with song through album to artist association."
    This reverts commit 46b8a211fdaeeb1f01c5a9eaac34216d8b7e4f52.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,9 +4,7 @@
 # validates name under maximum allowed characters
 #
 class Song < ActiveRecord::Base
+  belongs_to :artist
   belongs_to :album
-  has_one :artist, through: :album
-
   validates :title, presence: true, length: { maximum: 150 }
-  validates :album_id, presence: true
 end
