6b8b20a484792bbe003c38b3659c2a55c628abeb
Chet Bramble <chet@gooee.com>
2016-10-10 15:23:37 -0400

    Added current_user logic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Assign the current user
+  def current_user=(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  # Returns a boolean representing if the user is logged in
+  def logged_in?
+    !!current_user
+  end
+  helper_method :logged_in?
+
+  # Method to use in filter to ensure the user is logged in
+  def authenticate!
+    unless logged_in?
+      redirect_to login_path
+    end
+  end
 end
